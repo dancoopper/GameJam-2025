948b1d845107cfea3c66d8d7c963bcecf25a8473
Sokmontrey <sythatsokmontrey@gmail.com>
2025-08-01 23:12:02 -0400

    alway show player hand (avoid glitching at the portal)

--- addons/sphynx_portal_toolkit/players/head.gd
+++ addons/sphynx_portal_toolkit/players/head.gd
@@ -19,18 +19,6 @@ func _process(delta):
 			try_pickup()
 		else:
 			try_dropoff()
-
-	
-	#var object = raycast.get_collider()
-	#if raycast.is_colliding() or player_holding:
-		#if object.is_in_group("pickable"):
-			#if Input.is_action_pressed("Interact"):
-				#print("interact button pressed")
-				#object.global_position = hand.global_position
-				#object.global_rotation = hand.global_rotation
-				#object.collision_layer = 2
-				#
-				#object.linear_velocity = Vector3(0.1, 3, 0.1)
 			
 func try_pickup():
 	
