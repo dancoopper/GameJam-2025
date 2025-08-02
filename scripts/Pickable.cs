b9ae7ec4b022901b7081eef661811b853d6ac3b6
Sokmontrey <sythatsokmontrey@gmail.com>
2025-08-02 11:17:35 -0400

    update player collision

--- scripts/Pickable.cs
+++ scripts/Pickable.cs
@@ -5,31 +5,36 @@ using System.Runtime;
 public partial class Pickable : RigidBody3D
 {
 
-    #nullable enable
-    private Node3D? _target = null;
+	#nullable enable
+	private Node3D? _target = null;
+	private Vector3 _previousPosition;
+	private double _delta;
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (_target is not null)
-        {
-            Position = _target.GlobalPosition;
-            Rotation = _target.GlobalRotation;
-        }
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		if (_target is not null)
+		{
+			Position = _target.GlobalPosition;
+			Rotation = _target.GlobalRotation;
+			_previousPosition = Position;
+			_delta = delta;
+		}
+	}
 
-    public virtual void Pick(Node3D target)
-    {
-        _target = target;
-        this.CollisionLayer = 2;
-        LinearVelocity = Vector3.Zero;
-        AngularVelocity = Vector3.Zero;
-    }
+	public virtual void Pick(Node3D target)
+	{
+		_target = target;
+		this.CollisionLayer = 2;
+		LinearVelocity = Vector3.Zero;
+		AngularVelocity = Vector3.Zero;
+	}
 
 	public virtual void Drop()
 	{
-        this.CollisionLayer = 1;
-        LinearVelocity = new Vector3(0, 3, 0);
-        AngularVelocity = Vector3.Zero;
+		this.CollisionLayer = 1;
+		LinearVelocity = new Vector3(0, 3, 0);
+		//LinearVelocity = (Position - _previousPosition);
+		AngularVelocity = Vector3.Zero;
 		_target = null;
 	}
 }
